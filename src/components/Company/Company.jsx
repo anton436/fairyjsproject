9bc2863a60bee9b1adad55a492b7bfbbd659c1d5
Diana <dianadianadiana586@gmail.com>
2023-03-22 14:44:48 +0600

    add files

---
+++ src/components/Company/Company.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./company.css";
+import image1 from "../image1.png";
+
+const Company = () => {
+  return (
+    <div>
+      <img src={image1} alt="" />
+    </div>
+  );
+};
+
+export default Company;

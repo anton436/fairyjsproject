860695d39f039e897975e39ba94eefc5b6c2b23d
anton <oneshot436@gmail.com>
2023-03-23 10:38:36 +0600

    pulled

--- src/components/product/AddProduct.jsx
+++ src/components/product/AddProduct.jsx
@@ -1,20 +1,20 @@
-import { Box, Button, TextField } from "@mui/material";
-import React, { useState } from "react";
-import { useProducts } from "../../context/ProductContextProvider";
-import "./Product.css";
+import { Box, Button, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import { useProducts } from '../../contexts/ProductContextProvider';
+import './Product.css';
 
 const AddProduct = () => {
   const [product, setProduct] = useState({
-    name: "",
-    description: "",
+    name: '',
+    description: '',
     price: 0,
-    picture: "",
-    type: "",
+    picture: '',
+    type: '',
   });
   const { addProduct } = useProducts();
 
   const handleInp = (e) => {
-    if (e.target.name == "price") {
+    if (e.target.name == 'price') {
       let obj = {
         ...product,
         [e.target.name]: Number(e.target.value),
@@ -32,69 +32,69 @@ const AddProduct = () => {
   return (
     <Box
       sx={{
-        width: "40vw",
-        margin: "5vh auto",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
+        width: '40vw',
+        margin: '5vh auto',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
       }}
     >
       <TextField
-        className="admin_inp"
-        id="outlined-basic"
-        label="Name"
-        variant="outlined"
-        name="name"
-        size="small"
+        className='admin_inp'
+        id='outlined-basic'
+        label='Name'
+        variant='outlined'
+        name='name'
+        size='small'
         onChange={handleInp}
       />
       <TextField
-        className="admin_inp"
-        id="outlined-basic"
-        label="Descripton"
-        variant="outlined"
-        name="description"
-        size="small"
+        className='admin_inp'
+        id='outlined-basic'
+        label='Descripton'
+        variant='outlined'
+        name='description'
+        size='small'
         onChange={handleInp}
       />
       <TextField
-        className="admin_inp"
-        id="outlined-basic"
-        label="Price"
-        variant="outlined"
-        name="price"
-        size="small"
+        className='admin_inp'
+        id='outlined-basic'
+        label='Price'
+        variant='outlined'
+        name='price'
+        size='small'
         onChange={handleInp}
       />
       <TextField
-        className="admin_inp"
-        id="outlined-basic"
-        label="Picture"
-        variant="outlined"
-        name="picture"
-        size="small"
+        className='admin_inp'
+        id='outlined-basic'
+        label='Picture'
+        variant='outlined'
+        name='picture'
+        size='small'
         onChange={handleInp}
       />
       <TextField
-        className="admin_inp"
-        id="outlined-basic"
-        label="Type"
-        variant="outlined"
-        name="type"
-        size="small"
+        className='admin_inp'
+        id='outlined-basic'
+        label='Type'
+        variant='outlined'
+        name='type'
+        size='small'
         onChange={handleInp}
       />
       <Button
-        className="admin_btn"
+        className='admin_btn'
         onClick={() => addProduct(product)}
-        size="large"
-        variant="outlined"
+        size='large'
+        variant='outlined'
         sx={{
-          border: "1px solid black",
-          color: "white",
-          backgroundColor: "black",
-          fontFamily: "Montserrat, sans-serif",
+          border: '1px solid black',
+          color: 'white',
+          backgroundColor: 'black',
+          fontFamily: 'Montserrat, sans-serif',
         }}
       >
         Add product

5094139ea9994ba80ba57d2e0f4aae0009022b72
venusblue1708 <venusblue1708@gmail.com>
2023-03-27 12:06:24 +0600

    .

--- src/components/product/ProductList/ProductList.jsx
+++ src/components/product/ProductList/ProductList.jsx
@@ -5,14 +5,21 @@ import ProductCard from "../ProductCard";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 import "./ProductList.css";
+import { useSearchParams } from "react-router-dom";
 
 const ProductList = () => {
   const { getProducts, products } = useProducts();
 
+  const [searchParams] = useSearchParams();
   useEffect(() => {
     getProducts();
     // console.warn(products);
   }, []);
+  useEffect(() => {
+    // console.log(searchParams);
+    getProducts();
+    // console.warn(products);
+  }, [searchParams]);
   //! pagination
   const [page, setPage] = React.useState(1);
   const handleChange = (event, value) => {
--- src/contexts/ProductContextProvider.js
+++ src/contexts/ProductContextProvider.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { createContext, useContext, useReducer } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { API } from "../helpers/consts";
 
@@ -51,10 +52,11 @@ const reducer = (state = INIT_STATE, action) => {
 const ProductContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, INIT_STATE);
 
+  const navigate = useNavigate();
   //! read (get request)
 
   const getProducts = async () => {
-    const { data } = await axios.get(API);
+    const { data } = await axios.get(`${API}${window.location.search}`);
 
     dispatch({ type: "GET_PRODUCTS", payload: data });
   };
@@ -89,6 +91,18 @@ const ProductContextProvider = ({ children }) => {
     getProducts();
   };
 
+  const fetchByParams = async (query, value) => {
+    const search = new URLSearchParams(window.location.search);
+    if (value == "all") {
+      search.delete(query);
+    } else {
+      search.set(query, value);
+    }
+
+    const url = `${window.location.pathname}?${search.toString()}`;
+    navigate(url);
+  };
+
   const values = {
     saveEditedProduct,
     addProduct,
@@ -99,6 +113,7 @@ const ProductContextProvider = ({ children }) => {
     productDetails: state.productDetails,
     getRandomProducts,
     randomProducts: state.randomProducts,
+    fetchByParams,
   };
 
   return (

657316004b9b4b63d87c3f0a8352292f9484a4b6
Шераман Суеркулов <ssheraman@Seramans-MacBook-Pro.local>
2023-03-28 18:09:02 +0600

    newSideBar

--- src/components/product/SideBar.jsx
+++ src/components/product/SideBar.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import {
+  Box,
   FormControl,
   FormControlLabel,
   FormLabel,
@@ -11,18 +12,61 @@ import {
 } from "@mui/material";
 import { useSearchParams } from "react-router-dom";
 import { useProducts } from "../../contexts/ProductContextProvider";
+import { useAuth } from "../../contexts/AuthContextProvider";
 const SideBar = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [search, setSearch] = useState(searchParams.get("q") || "");
   const { fetchByParams, getProducts } = useProducts();
+
   useEffect(() => {
     setSearchParams({ q: search });
     getProducts();
   }, [search]);
 
+  const { user } = useAuth();
+  console.log(user);
   return (
-    <Grid item md={4}>
-      <Paper elevation={1} sx={{ width: "200px", boxShadow: "none" }}>
+    <Box item md={4}>
+      <Box
+        elevation={1}
+        sx={{
+          width: "max-content",
+          minWidth: "200px",
+          height: "min-content",
+          border: "2px solid rgb(254, 249, 239)",
+          boxShadow:
+            "0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12)",
+          marginTop: "7%",
+        }}
+      >
+        <Box
+          style={{
+            display: "flex",
+            flexDirection: "column",
+
+            alignItems: "center",
+          }}
+        >
+          {user.email ? (
+            <img
+              style={{
+                borderRadius: "100%",
+                height: " 100px",
+                width: "100px",
+                objectFit: "cover",
+                marginTop: "8px",
+              }}
+              src="https://i.pinimg.com/564x/59/73/ff/5973ff63055dc63a35e317d4ac3b08e0.jpg"
+              alt=""
+            />
+          ) : null}
+          <FormLabel
+            style={{ marginTop: "8px" }}
+            id="demo-radio-buttons-group-label"
+          >
+            {user.email}
+          </FormLabel>
+        </Box>
         <TextField
           id="standard-basic"
           label="Search"
@@ -32,7 +76,7 @@ const SideBar = () => {
           value={search}
         />
 
-        <Grid>
+        <Box style={{ marginLeft: "9%", marginTop: "20px" }}>
           <FormControl>
             <FormLabel id="demo-radio-buttons-group-label">Type</FormLabel>
             <RadioGroup
@@ -51,8 +95,8 @@ const SideBar = () => {
               />
             </RadioGroup>
           </FormControl>
-        </Grid>
-        <Grid>
+        </Box>
+        <Box style={{ marginLeft: "9%", marginBottom: "20px" }}>
           <FormControl>
             <FormLabel id="demo-radio-buttons-group-label">Price</FormLabel>
             <RadioGroup
@@ -79,9 +123,9 @@ const SideBar = () => {
               />
             </RadioGroup>
           </FormControl>
-        </Grid>
-      </Paper>
-    </Grid>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
